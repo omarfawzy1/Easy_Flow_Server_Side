7115b837f27a82805728a677c82bac3623f70d1c
Omar Mohamed <60580509+omarfawzy1@users.noreply.github.com>
2022-12-12 21:13:11 +0200

    init

---
+++ src/main/java/com/example/easy_flow_backend/EasyFlowBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.easy_flow_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyFlowBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyFlowBackendApplication.class, args);
+    }
+
+}
