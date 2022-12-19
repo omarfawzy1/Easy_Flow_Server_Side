a228a692b8394c66d4f1e8c3b0f03d54ad9e9557
Abdelaziz-Gbr <84877625+Abdelaziz-Gbr@users.noreply.github.com>
2022-12-19 18:16:16 +0200

    added the crossorigin annotation to the controllers

--- src/main/java/com/example/easy_flow_backend/controller/ProfileController.java
+++ src/main/java/com/example/easy_flow_backend/controller/ProfileController.java
@@ -1,10 +1,12 @@
 package com.example.easy_flow_backend.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@CrossOrigin
 @RequestMapping("profile")
 public class ProfileController {
 
