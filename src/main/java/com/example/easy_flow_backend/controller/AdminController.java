6a2cb010730e3b859fd7d5e0b346d8a0a115aa6e
Omar Mohamed <60580509+omarfawzy1@users.noreply.github.com>
2023-02-03 18:34:54 +0200

    Get All Users AdminController.java

--- src/main/java/com/example/easy_flow_backend/controller/AdminController.java
+++ src/main/java/com/example/easy_flow_backend/controller/AdminController.java
@@ -1,6 +1,7 @@
 package com.example.easy_flow_backend.controller;
 
 import com.example.easy_flow_backend.entity.Passenger;
+import com.example.easy_flow_backend.repos.PassengersRepo;
 import com.example.easy_flow_backend.service.AdminService;
 import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -23,7 +25,10 @@ public class AdminController {
         Users Page :
             Username, Email, Type(Military, Student, etc), Gender, Phone Number
      */
-
+    @GetMapping("get/users")
+    public List<Passenger> getUsers(){
+        return passengersRepo.findAll();
+    }
     /*
     Todo server side
         Analytics Module
@@ -31,9 +36,12 @@ public class AdminController {
      */
     @Autowired
     private AdminService adminService;
+    @Autowired
+    private PassengersRepo passengersRepo;
+
     @GetMapping("index")
     public String index(){
-        return "a7a";
+        return "";
     }
     @GetMapping("passengers")
     public ResponseEntity<List<Passenger>> getAllPassangers(){
